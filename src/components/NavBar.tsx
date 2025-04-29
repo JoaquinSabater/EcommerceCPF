388d3b8b5c477523ea8d30c1eb805db3eb6b82c2
JoaquinSabater <joaquinsabater15@gmail.com>
2025-04-29 17:25:09 -0300

    Cambios esteticos en la navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,13 +6,15 @@ import Image from 'next/image';
 import MobileMenu from './MobileMenu';
 import Search, { SearchSkeleton } from './Search';
 
-const SITE_NAME = 'Mi Tienda';
+const SITE_NAME = '';
 
 export default function NavBar() {
   const menu = [
-    { title: 'Inicio', path: '/' },
-    { title: 'Productos', path: '/products' },
-    { title: 'Contacto', path: '/contact' },
+    { title: 'Baterias', path: '/baterias' },
+    { title: 'Cables', path: '/cables' },
+    { title: 'Vidrios', path: '/vidrios' },
+    { title: 'Fundas', path: '/fundas' },
+    { title: 'Accesorios', path: '/accesorios' },
   ];
 
   return (
@@ -25,59 +27,58 @@ export default function NavBar() {
       </div>
 
       {/* Logo + Links */}
-      <div className="flex w-full items-center">
-        <div className="flex w-full md:w-1/3">
-          <Link
-            href="/"
-            className="mr-2 flex w-full items-center justify-center md:w-auto lg:mr-6"
-          >
-            <Image
-              src="/logo_orange_on_transparent.png"
-              width={40}
-              height={40}
-              alt="logo"
-            />
-            <div className="ml-2 flex-none text-sm font-medium uppercase md:hidden lg:block text-black dark:text-white">
-              {SITE_NAME}
-            </div>
-          </Link>
+      <div className="absolute left-1/2 transform -translate-x-1/2 md:static md:translate-x-0 md:flex md:w-1/3 md:items-center">
+        <Link
+          href="/"
+          className="flex items-center justify-center"
+        >
+          <Image
+            src="/logo_orange_on_transparent.png"
+            width={40}
+            height={40}
+            alt="logo"
+          />
+          <div className="ml-2 hidden text-sm font-medium uppercase lg:block text-black dark:text-white">
+            {SITE_NAME}
+          </div>
+        </Link>
 
-          <ul className="hidden gap-6 text-sm md:flex md:items-center">
-            {menu.map((item) => (
-              <li key={item.title}>
-                <Link
-                  href={item.path}
-                  className="text-neutral-500 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300"
-                >
-                  {item.title}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
+        {/* Desktop links */}
+        <ul className="hidden md:flex md:items-center gap-6 text-sm ml-4">
+          {menu.map((item) => (
+            <li key={item.title}>
+              <Link
+                href={item.path}
+                className="text-neutral-500 underline-offset-4 hover:text-black hover:underline dark:text-neutral-400 dark:hover:text-neutral-300"
+              >
+                {item.title}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
 
-        {/* Search */}
-        <div className="hidden justify-center md:flex md:w-1/3">
-          <Suspense fallback={<SearchSkeleton />}>
-            <Search />
-          </Suspense>
-        </div>
+      {/* Search */}
+      <div className="hidden md:flex md:justify-center md:w-1/3">
+        <Suspense fallback={<SearchSkeleton />}>
+          <Search />
+        </Suspense>
+      </div>
 
-        {/* Cart */}
-        <div className="flex justify-end md:w-1/3">
-          <button className="relative">
-            <Image
-              src="/cart.svg"
-              width={40}
-              height={40}
-              alt="shopping cart icon"
-              className="dark:invert"
-            />
-            <div className="rounded-full flex justify-center items-center bg-orange-600 text-xs text-white absolute w-6 h-5 bottom-6 -right-1">
-              0
-            </div>
-          </button>
-        </div>
+      {/* Cart */}
+      <div className="flex items-center justify-end md:w-1/3">
+        <button className="relative">
+          <Image
+            src="/cart.svg"
+            width={30}
+            height={30}
+            alt="shopping cart icon"
+            className="dark:invert"
+          />
+          <div className="rounded-full flex justify-center items-center bg-orange-600 text-xs text-white absolute w-5 h-5 -top-2 -right-2">
+            0
+          </div>
+        </button>
       </div>
     </nav>
   );
